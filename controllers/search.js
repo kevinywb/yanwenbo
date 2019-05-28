e0578646d0ae3fcbaaa8ba94b2a990f64386d4a0
kevinywb <kevinywb@vip.qq.com>
2019-05-28 18:37:09 +0800

    create a new version

---
+++ controllers/search.js
@@ -0,0 +1,6 @@
+exports.index = function (req, res, next) {
+  var q = req.query.q;
+  q = encodeURIComponent(q);
+  res.redirect(`https://cn.bing.com/search?q=${q}`);
+  // res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
+};
