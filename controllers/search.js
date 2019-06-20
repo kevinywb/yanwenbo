0e8e66fd7307e11a091cf931f808936c6b9cc138
kevinywb <kevinywb@vip.qq.com>
2019-06-20 15:16:22 +0800

    switch to baidu searching

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,7 @@
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect(`https://cn.bing.com/search?q=site:yanwenbo.cn+${q}`);
+  res.redirect(`https://www.baidu.com/s?wd=site%3Ayanwenbo.cn%20${q}`);
+  // res.redirect(`https://cn.bing.com/search?q=site:yanwenbo.cn+${q}`);
   // res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
 };
